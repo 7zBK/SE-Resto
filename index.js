c3f8204bd47e5c3167cedcfe60fc5b49de332d56
7zBK <PrempreeEz@gmail.com>
2023-03-09 10:45:06 +0700

    complet se-resto

--- index.js
+++ index.js
@@ -60,3 +60,11 @@ var swiper = new Swiper(".review-slider",{
     }
   }
 });
+/**Loader section**/
+function loader(){
+  document.querySelector(".loader-container").classList.add("fade-out")
+}
+function fadeOut(){
+  setInterval(loader, 3000)
+}
+window.onload = fadeOut;
